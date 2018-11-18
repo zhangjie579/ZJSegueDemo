083b5286f5c58307dbcbc3916c6eb3bb13f24017
张杰 <527512749@qq.com>
2018-11-18 10:48:56 +0800

    【add】添加版本1的log

--- 半糖demo/003 - 半糖demo/AppDelegate.m
+++ 半糖demo/003 - 半糖demo/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"%@", @"我是版本1");
     // Override point for customization after application launch.
     return YES;
 }
