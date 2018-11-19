2c4832c2ed37879383543c31ff80fec92bec1362
zhangjie <527512749@qq.com>
2018-11-19 10:01:43 +0800

    【edit】版本2

--- 半糖demo/003 - 半糖demo/AppDelegate.m
+++ 半糖demo/003 - 半糖demo/AppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     NSLog(@"%@", @"我是版本1");
-    NSLog(@"%@", @"我是版本2");
     // Override point for customization after application launch.
     return YES;
 }
